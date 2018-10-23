decd992c7c07828cf2c076f045632eea323673f9
classicalliu <classicalliu@gmail.com>
2018-10-23 12:04:23 +0800

    reset sidekiq_pid dir

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -3,3 +3,4 @@ set :deploy_to, '/home/deploy/ether-bridge-server'
 set :branch, 'develop'
 set :user, 'root'
 set :rvm_use_path, '/usr/share/rvm/scripts/rvm'
+set :sidekiq_pid, "#{fetch(:deploy_to)}/shared/tmp/pids/sidekiq.pid"
